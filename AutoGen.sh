395a5020efedd187e7e4c6cb5f665212ae3bad55
Twistedone73 <rafifadilah2016@gmail.com>
2025-04-19 23:00:30 +0700

    Sudah Selesai

---
+++ AutoGen.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+	echo "Membuat Struktur Folder"
+	mkdir BackEnd FrontEnd
+
+	echo "Membuat Struktur Folder1 dan file"
+	touch FrontEnd/{Sigma.html,Yapping.css,Skibdi.js}
+
+	echo "Membuat Struktur Folder2 dan file"
+	touch BackEnd/{index.html,style.css,dashboard.html,login.html}
+	
+	echo " ....Selesai" 
